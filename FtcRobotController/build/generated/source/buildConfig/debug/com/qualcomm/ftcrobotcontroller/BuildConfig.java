a3140aa89dbfd8a7a196c984439fa8c639b5a59e
NRobowarriors <56520176+NRobowarriors@users.noreply.github.com>
2022-10-16 14:10:36 -0500

    yes

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2022-10-02T16:48:00.271-0500";
+  public static final String APP_BUILD_TIME = "2022-10-16T14:06:18.075-0500";
 }
