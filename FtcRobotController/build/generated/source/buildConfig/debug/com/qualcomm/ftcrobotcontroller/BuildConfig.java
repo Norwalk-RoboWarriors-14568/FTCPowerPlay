18a64bc6361bd3cde7431b9119677bf633fb9af0
NRobowarriors <realnorwalk@gmail.com>
2022-10-23 16:55:54 -0500

    aa

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -10,8 +10,12 @@ public final class BuildConfig {
   // Field from default config.
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
   public static final String APP_BUILD_TIME = "2022-10-23T14:19:47.392-0500";
+=======
+  public static final String APP_BUILD_TIME = "2022-10-23T16:36:07.703-0500";
+>>>>>>> Stashed changes
 =======
   public static final String APP_BUILD_TIME = "2022-10-23T16:36:07.703-0500";
 >>>>>>> Stashed changes
